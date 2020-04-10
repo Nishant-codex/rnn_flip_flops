db64591957ec3abfe15d65d9616c9e12f3ecdf6b
Nishant-codex <nishant.joshi@campus.tu-berlin.de>
2020-04-10 22:53:06 +0200

    updated runfiles for running end to end training and fixed points

--- fixed_point_edits/runfiles.py
+++ fixed_point_edits/runfiles.py
@@ -24,28 +24,36 @@ import matplotlib.pyplot as plt
 np.random.seed(400)
 # import numpy as np
 #import time
-# from AdaptiveGradNormClip import AdaptiveGradNormClip
-# from AdaptiveLearningRate import AdaptiveLearningRate
+from AdaptiveGradNormClip import AdaptiveGradNormClip
+from AdaptiveLearningRate import AdaptiveLearningRate
 
 from FlipFlop import FlipFlop
-# from FlipFlop_GPU import FlipFlop_GPU
+from FlipFlop_GPU import FlipFlop_GPU
 from FixedPointSearch import *
 
 
-flip = FlipFlop()
-flip.data_batches(10000)
-# n_bits = 3
+n_bits = 3
 
-# rnn_object = FlipFlop(c_type='GRU')
-# # rnn_object.train_network(10, save=True)
-# lis = rnn_object.reload_from_checkpoints('/home/joshi/fixed_point_edits/saver/GRU_7000_epochs')
-# hid = lis['hiddens']
+rnn_object = FlipFlop(c_type='GRU')
 
-# inputs = np.zeros([1,n_bits])
-# fps = FixedPointSearch('GRU',
-# 						hid,
-# 						cell=rnn_object.graph['cell'],
-# 						sess=rnn_object.sess)
-# sample = fps.sample_states(1000,hid,'GRU',0.04)
-# fps_unique, fps_all = fps.find_fixed_points(inputs, save = True)
+rnn_object.data_batches(80000, 8, save=True)
+print('created the dataset')
+
+rnn_object.train_network(save=True)
+
+lis = rnn_object.reload_from_checkpoints(os.getcwd()+'/GRU_hps(64_0.01)/')
+
+plt.plot(lis['predictions'][:500,0])
+plt.plot(np.reshape(lis['truth'],[-1,3])[:500,0])
+
+plt.show()
+plt.savefig('outputs.png')
+
+inputs = np.zeros([1,n_bits])
+fps = FixedPointSearch('GRU',
+						lis['hiddens'],
+						cell=rnn_object.graph['cell'],
+						sess=rnn_object.sess)
+sample = fps.sample_states(1000,lis['hiddens'],'GRU',0.04)
+fps_unique, fps_all = fps.find_fixed_points(inputs, save = True)
 
